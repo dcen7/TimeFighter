7098a2740b9940fcf90654d8732f6795d91aa2ca
dcen7 <mdcengiz@gmail.com>
2021-10-29 08:04:13 -0500

    added count down timer

--- src/main/java/com/example/timefighter/MainActivity.kt
+++ src/main/java/com/example/timefighter/MainActivity.kt
@@ -5,9 +5,16 @@ import android.os.Bundle
 import android.os.CountDownTimer
 import android.widget.Button
 import android.widget.TextView
+import android.widget.Toast
 
 class MainActivity : AppCompatActivity() {
     internal var score = 0
+
+    internal var gameStarted = false
+
+    internal lateinit var countDownTimer: CountDownTimer
+    internal val initialCountDown: Long = 60000
+    internal val countDownInterval: Long = 1000
 
     internal lateinit var tapMeButton: Button
     internal lateinit var gameScoreTextView: TextView
@@ -23,15 +30,48 @@ class MainActivity : AppCompatActivity() {
         tapMeButton.setOnClickListener { view ->
             incrementScore()
         }
+
+        resetGame()
+    }
+
+    private fun resetGame() {
+        score = 0
         gameScoreTextView.text = getString(R.string.yourScore, score)
 
+        val initialTimeLeft = initialCountDown / 1000
+        timeLeftTextView.text = getString(R.string.timeLeft, initialTimeLeft)
+
+        countDownTimer = object : CountDownTimer(initialCountDown, countDownInterval) {
+            override fun onTick(millisUntilFinished: Long) {
+                val timeLeft = millisUntilFinished / 1000
+                timeLeftTextView.text = getString(R.string.timeLeft, timeLeft)
+            }
+
+            override fun onFinish() {
+                endGame()
+            }
+        }
+
+        gameStarted = false
+    }
+
+    private fun startGame() {
+        countDownTimer.start()
+        gameStarted = true
     }
 
     private fun incrementScore() {
-        score =+ 1
+        if(!gameStarted) {
+            startGame()
+        }
+        score += 1
         val newScore = getString(R.string.yourScore, score)
         gameScoreTextView.text = newScore
     }
 
+    private fun endGame() {
+        Toast.makeText(this, getString(R.string.gameOverMessage, score), Toast.LENGTH_LONG).show()
+        resetGame()
+    }
 
 }
